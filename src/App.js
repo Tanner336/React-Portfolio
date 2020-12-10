f080c39b8f4961e1446f229cb99fa37efb3f79e3
Tanner Cook <tannercook336@gmail.com>
2020-12-09 22:45:59 -0500

    removed nesting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./pages/Navbar/Navbar";
+import Footer from "./pages/Footer/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
